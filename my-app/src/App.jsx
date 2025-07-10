709c6867e01f373372c8e7a76ae08f8200b86469
Ermiyas-T <jermiket@gmail.com>
2025-07-10 11:03:51 +0300

    added Navbar

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages/Home";
+import Footer from "./pages/footer";
 const App = () => {
   return (
     <div>
       <Home />
+      <Footer />
     </div>
   );
 };
-
+// some comment
 export default App;
